cfcbb338413bba12ee6de8cf2be8c2527afe543e
bismarazi02 <157125512+bismarazi02@users.noreply.github.com>
2024-01-20 18:05:10 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -140,3 +140,5 @@ def preprocess(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,
 # if feedback:
 #   st.header("Thank you for rating the app!")
 #   st.info("Caution: This is just a prediction and not doctoral advice. Kindly see a doctor if you feel the symptoms persist.")
+
+
