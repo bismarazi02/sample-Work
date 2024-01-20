24c6f200e61d9a9558511083c985966f5c5b523a
bismarazi02 <157125512+bismarazi02@users.noreply.github.com>
2024-01-20 20:32:52 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ Original file is located at
 
 # Commented out IPython magic to ensure Python compatibility.
 # %%writefile healthy-heart-app.py
-
+pip install --upgrade pip
 import streamlit as st
 import base64
 import numpy as np
