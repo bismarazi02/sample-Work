0b23aefd111da90463df9b96a56f5691b22c9980
bismarazi02 <157125512+bismarazi02@users.noreply.github.com>
2024-01-20 18:14:36 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,61 +16,62 @@ import sklearn
 import numpy as np
 import pickle as pkl
 from sklearn.preprocessing import MinMaxScaler
-scal=MinMaxScaler()
+scal = MinMaxScaler()
 
-model=pkl.load(open("final_model.p","rb"))
+model = pkl.load(open("final_model.p", "rb"))
 
-st.set_page_config(page_title="Healthy Heart App",page_icon="⚕️",layout="centered",initial_sidebar_state="expanded")
-def preprocess(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal ):
-     if sex=="male":
-        sex=1
-     else:
-        sex=0
+st.set_page_config(page_title="Healthy Heart App", page_icon="⚕️", layout="centered", initial_sidebar_state="expanded")
 
-    if cp=="Typical angina":
-        cp=0
-    elif cp=="Atypical angina":
-        cp=1
-    elif cp=="Non-anginal pain":
-        cp=2
-    elif cp=="Asymptomatic":
-        cp=2
+def preprocess(age, sex, cp, trestbps, restecg, chol, fbs, thalach, exang, oldpeak, slope, ca, thal):
+    if sex == "male":
+        sex = 1
+    else:
+        sex = 0
 
-    if exang=="Yes":
-        exang=1
-    elif exang=="No":
-        exang=0
+    if cp == "Typical angina":
+        cp = 0
+    elif cp == "Atypical angina":
+        cp = 1
+    elif cp == "Non-anginal pain":
+        cp = 2
+    elif cp == "Asymptomatic":
+        cp = 2
 
-    if fbs=="Yes":
-        fbs=1
-    elif fbs=="No":
-        fbs=0
+    if exang == "Yes":
+        exang = 1
+    elif exang == "No":
+        exang = 0
 
-    if slope=="Upsloping: better heart rate with excercise(uncommon)":
-        slope=0
-    elif slope=="Flatsloping: minimal change(typical healthy heart)":
-        slope=1
-    elif slope=="Downsloping: signs of unhealthy heart":
-        slope=2
+    if fbs == "Yes":
+        fbs = 1
+    elif fbs == "No":
+        fbs = 0
 
-    if thal=="fixed defect: used to be defect but ok now":
-        thal=6
-    elif thal=="reversable defect: no proper blood movement when excercising":
-        thal=7
-    elif thal=="normal":
-        thal=2.31
+    if slope == "Upsloping: better heart rate with exercise(uncommon)":
+        slope = 0
+    elif slope == "Flatsloping: minimal change(typical healthy heart)":
+        slope = 1
+    elif slope == "Downsloping: signs of unhealthy heart":
+        slope = 2
 
-    if restecg=="Nothing to note":
-        restecg=0
-    elif restecg=="ST-T Wave abnormality":
-        restecg=1
-    elif restecg=="Possible or definite left ventricular hypertrophy":
-        restecg=2
+    if thal == "fixed defect: used to be defect but ok now":
+        thal = 6
+    elif thal == "reversable defect: no proper blood movement when exercising":
+        thal = 7
+    elif thal == "normal":
+        thal = 2.31
 
-    user_input=[age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal]
-    user_input=np.array(user_input)
-    user_input=user_input.reshape(1,-1)
-    user_input=scal.fit_transform(user_input)
+    if restecg == "Nothing to note":
+        restecg = 0
+    elif restecg == "ST-T Wave abnormality":
+        restecg = 1
+    elif restecg == "Possible or definite left ventricular hypertrophy":
+        restecg = 2
+
+    user_input = [age, sex, cp, trestbps, restecg, chol, fbs, thalach, exang, oldpeak, slope, ca, thal]
+    user_input = np.array(user_input)
+    user_input = user_input.reshape(1, -1)
+    user_input = scal.fit_transform(user_input)
     prediction = model.predict(user_input)
 
     return prediction
@@ -84,21 +85,21 @@ html_temp = """
 st.markdown(html_temp, unsafe_allow_html=True)
 st.subheader('by Amlan Mohanty ')
 
-age=st.selectbox("Age", range(1,121,1))
+age = st.selectbox("Age", range(1, 121, 1))
 sex = st.radio("Select Gender: ", ('male', 'female'))
 cp = st.selectbox('Chest Pain Type',("Typical angina","Atypical angina","Non-anginal pain","Asymptomatic"))
-trestbps=st.selectbox('Resting Blood Sugar', range(1,500,1))
-restecg=st.selectbox('Resting Electrocardiographic Results',("Nothing to note","ST-T Wave abnormality","Possible or definite left ventricular hypertrophy"))
-chol=st.selectbox('Serum Cholestoral in mg/dl', range(1,1000,1))
-fbs=st.radio("Fasting Blood Sugar higher than 120 mg/dl", ['Yes','No'])
-thalach=st.selectbox('Maximum Heart Rate Achieved', range(1,300,1))
-exang=st.selectbox('Exercise Induced Angina',["Yes","No"])
-oldpeak=st.number_input('Oldpeak')
-slope = st.selectbox('Heart Rate Slope',("Upsloping: better heart rate with excercise(uncommon)","Flatsloping: minimal change(typical healthy heart)","Downsloping: signs of unhealthy heart"))
-ca=st.selectbox('Number of Major Vessels Colored by Flourosopy', range(0,5,1))
-thal=st.selectbox('Thalium Stress Result', range(1,8,1))
+trestbps = st.selectbox('Resting Blood Sugar', range(1, 500, 1))
+restecg = st.selectbox('Resting Electrocardiographic Results',("Nothing to note","ST-T Wave abnormality","Possible or definite left ventricular hypertrophy"))
+chol = st.selectbox('Serum Cholestoral in mg/dl', range(1, 1000, 1))
+fbs = st.radio("Fasting Blood Sugar higher than 120 mg/dl", ['Yes','No'])
+thalach = st.selectbox('Maximum Heart Rate Achieved', range(1, 300, 1))
+exang = st.selectbox('Exercise Induced Angina',["Yes","No"])
+oldpeak = st.number_input('Oldpeak')
+slope = st.selectbox('Heart Rate Slope',("Upsloping: better heart rate with exercise(uncommon)","Flatsloping: minimal change(typical healthy heart)","Downsloping: signs of unhealthy heart"))
+ca = st.selectbox('Number of Major Vessels Colored by Fluoroscopy', range(0, 5, 1))
+thal = st.selectbox('Thalium Stress Result', range(1, 8, 1))
 
-pred=preprocess(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal)
+pred = preprocess(age, sex, cp, trestbps, restecg, chol, fbs, thalach, exang, oldpeak, slope, ca, thal)
 
 if st.button("Predict"):
     if pred[0] == 0:
@@ -110,4 +111,3 @@ st.sidebar.subheader("About App")
 st.sidebar.info("This web app helps you find out whether you are at risk of developing a heart disease.")
 st.sidebar.info("Enter the required fields and click on the 'Predict' button to check whether you have a healthy heart")
 st.sidebar.info("Don't forget to rate this app")
-
