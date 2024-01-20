2aee55944f416c435477facf5fdd12d5c9a8dc1b
bismarazi02 <157125512+bismarazi02@users.noreply.github.com>
2024-01-20 21:55:51 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ Original file is located at
 
 # Commented out IPython magic to ensure Python compatibility.
 # %%writefile healthy-heart-app.py
+pip install scikit-learn --upgrade
 import streamlit as st
 import base64
 import numpy as np
