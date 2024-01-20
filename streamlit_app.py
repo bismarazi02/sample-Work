14dac11e1b3c89c4c32f252ab574a7a3457fcc41
bismarazi02 <157125512+bismarazi02@users.noreply.github.com>
2024-01-21 01:02:31 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,13 +70,11 @@ def preprocess(age, sex, cp, trestbps, restecg, chol, fbs, thalach, exang, oldpe
         restecg = 1
     elif restecg == "Possible or definite left ventricular hypertrophy":
         restecg = 2
-
- user_input=[age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal]
-    user_input=np.array(user_input)
-    user_input=user_input.reshape(1,-1)
-    user_input=scal.fit_transform(user_input)
-    prediction = model.predict(user_input)
-
+        user_input=[age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal]
+        user_input=np.array(user_input)
+        user_input=user_input.reshape(1,-1)
+        user_input=scal.fit_transform(user_input)
+        prediction = model.predict(user_input)
     return prediction
 
 html_temp = """
